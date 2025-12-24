27681a66172c6bf5b9bc6265977b7c9104e71a1b
roey_averbuch <roey6922@gmail.com>
2025-12-24 12:22:55 +0200

    adding field validation in the basemodel schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , Field
 
 class Contact(BaseModel):
-    first_name:str
-    last_name:str
-    phone_number:str
+    first_name:str = Field(min_length=1,max_length=50)
+    last_name:str = Field(min_length=1,max_length=50)
+    phone_number:str = Field(min_length=7,max_length=50)
 
 class ContactUpdate(BaseModel):
     first_name: str | None = None
