9eac12002e76b50062f6ab90c11ef3ca9bda5474
roey_averbuch <roey6922@gmail.com>
2025-12-24 12:26:39 +0200

    fixing previous commit

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,12 @@
-from pydantic import BaseModel , Field
+from pydantic import BaseModel, Field
 
 class Contact(BaseModel):
-    first_name:str = Field(min_length=1,max_length=50)
-    last_name:str = Field(min_length=1,max_length=50)
-    phone_number:str = Field(min_length=7,max_length=50)
+    first_name: str = Field(min_length=1, max_length=50)
+    last_name: str = Field(min_length=1, max_length=50)
+    phone_number: str = Field(min_length=7, max_length=20)
+
 
 class ContactUpdate(BaseModel):
-    first_name: str | None = None
-    last_name: str | None = None
-    phone_number: str | None = None 
+    first_name: str | None = Field(default=None, min_length=1, max_length=50)
+    last_name: str | None = Field(default=None, min_length=1, max_length=50)
+    phone_number: str | None = Field(default=None, min_length=7, max_length=20)
